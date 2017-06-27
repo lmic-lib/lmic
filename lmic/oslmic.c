f4f86fb27ad9fc85eda5e23952f674061a3b5447
Matthijs Kooijman <matthijs@stdin.nl>
2017-06-27 15:34:00 +0200

    Add additional debug output at level 2

--- lmic/oslmic.c
+++ lmic/oslmic.c
@@ -26,6 +26,7 @@
  */
 
 #include "lmic.h"
+#include <stdio.h>
 
 // RUNTIME STATE
 static struct {
@@ -58,8 +59,12 @@ static int unlinkjob (osjob_t** pnext, osjob_t* job) {
 // clear scheduled job
 void os_clearCallback (osjob_t* job) {
     hal_disableIRQs();
-    unlinkjob(&OS.scheduledjobs, job) || unlinkjob(&OS.runnablejobs, job);
+    u1_t res = unlinkjob(&OS.scheduledjobs, job) || unlinkjob(&OS.runnablejobs, job);
     hal_enableIRQs();
+    #if LMIC_DEBUG_LEVEL > 1
+        if (res)
+            lmic_printf("%lu: Cleared job %p\n", os_getTime(), job);
+    #endif
 }
 
 // schedule immediately runnable job
@@ -75,6 +80,9 @@ void os_setCallback (osjob_t* job, osjobcb_t cb) {
     for(pnext=&OS.runnablejobs; *pnext; pnext=&((*pnext)->next));
     *pnext = job;
     hal_enableIRQs();
+    #if LMIC_DEBUG_LEVEL > 1
+        lmic_printf("%lu: Scheduled job %p, cb %p ASAP\n", os_getTime(), job, cb);
+    #endif
 }
 
 // schedule timed job
@@ -97,6 +105,9 @@ void os_setTimedCallback (osjob_t* job, ostime_t time, osjobcb_t cb) {
     }
     *pnext = job;
     hal_enableIRQs();
+    #if LMIC_DEBUG_LEVEL > 1
+        lmic_printf("%lu: Scheduled job %p, cb %p at %lu\n", os_getTime(), job, cb, time);
+    #endif
 }
 
 // execute jobs from timer and from run queue
@@ -109,10 +120,12 @@ void os_runloop () {
 void os_runloop_once() {
     osjob_t* j = NULL;
     hal_disableIRQs();
+    bit_t is_runnable = 0;
     // check for runnable jobs
     if(OS.runnablejobs) {
         j = OS.runnablejobs;
         OS.runnablejobs = j->next;
+        is_runnable = 1;
     } else if(OS.scheduledjobs && hal_checkTimer(OS.scheduledjobs->deadline)) { // check for expired timed jobs
         j = OS.scheduledjobs;
         OS.scheduledjobs = j->next;
@@ -121,6 +134,9 @@ void os_runloop_once() {
     }
     hal_enableIRQs();
     if(j) { // run job callback
+        #if LMIC_DEBUG_LEVEL > 1
+            lmic_printf("%lu: Running job %p, cb %p, deadline %lu\n", os_getTime(), j, j->func, is_runnable ? 0 : j->deadline);
+        #endif
         j->func(j);
     }
 }
